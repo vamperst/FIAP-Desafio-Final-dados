c1dce38a911089e2ff56a7109c65f365ddd47c2d
rafael barbosa <macbook@MacBook-Pro-de-Rafael.local>
2025-04-09 09:06:38 -0300

    delete files

--- disponibilizando-os-dados/Crawler/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "glue_crawler_name" {
-  description = "Nome do Glue Crawler criado"
-  value       = aws_glue_crawler.s3_crawler.name
-}
-
-output "athena_workgroup_name" {
-  description = "Nome do Workgroup do Athena criado"
-  value       = aws_athena_workgroup.athena_workgroup.name
-}
